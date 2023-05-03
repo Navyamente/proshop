c4fbc2d1e7f74fed8cf91df0fdbd16d21fa92019
Abhilash Mandula <mandula.abhilash@gmail.com>
2023-05-03 14:02:11 +0530

    Fix router issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
@@ -10,14 +10,10 @@ const App = () => {
     <Router>
       <Header />
       <div className="py-3">
-        <Switch>
-          <Route path="/product/:id" exact>
-            <ProductScreen />
-          </Route>
-          <Route path="/" exact>
-            <HomeScreen />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
       </div>
       <Footer />
     </Router>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const ProductScreen = () => {
-  return <div>ProductScreen</div>;
+  const location = useLocation();
+  const product = location.state.product;
+
+  return (
+    <div>
+      <h1>Product {product._id}</h1>
+    </div>
+  );
 };
 
 export default ProductScreen;
